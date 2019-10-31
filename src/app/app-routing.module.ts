e855ab0c323fbb520c5630c0d6ebb9ae34a375e8
Hugo Gaio Shiroma <hshiroma@meuacesso.com.br>
2019-10-31 20:24:30 -0300

    chat component and text correction

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ChatComponent } from './components/chat/chat.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'app-chat',
+    component: ChatComponent,
+    data: { title: 'Home' }
+  },
+  {
+    path: 'welcome',
+    component: AppComponent,
+    data: { title: 'Home' }
+  },
+  { path: '',
+    redirectTo: '/welcome',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
